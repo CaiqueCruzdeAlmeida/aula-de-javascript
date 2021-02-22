063cdf5ac3eb697cdd2ebec4c47ce2c3cabdddc8
CaiqueCruzdeAlmeida <caique.10almeida@gmail.com>
2021-02-22 15:02:57 -0300

    aula java script

---
+++ js/main.js
@@ -0,0 +1,100 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado<b>";
+   
+   
+    //console.log(document.getElementById("agradecimento"));
+    
+    //lert("Obrigado");
+}
+function redirecionar(){
+    //window.open("https://onepieceex.net/");
+    window.location.href = "https://onepieceex.net/";
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado"
+    elemento.innerHTML = "obrigado";
+    //alert("trocar texto")
+}
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "passe o mouse aqui";
+    elemento.innerHTML = "passe o mouse aqui";
+}
+function load(){
+    alert("página carregada");
+}
+
+//function soma(n1, n2){
+   // return n1 + n2;
+
+//}
+//function validaIdade(idade){
+  // var validar;
+   // if (idade >=18){
+   //      var validar = true
+   // }else{
+   //     var validar= false;
+   // }
+   // return validar;
+//}
+//var idade = prompt("qual sua idade?")
+//console.log(validaIdade(idade));
+
+
+//alert(soma(5,10));
+
+//function setReplace(frase, nome, novo_nome){
+  //  return frase.replace(nome, novo_nome)
+//}
+ 
+//alert(soma(5,10));
+//alert(setReplace("Vai santos", "santos", "Brasil"));
+
+
+
+//var d = new Date();
+//alert(d.getMonth()+1);
+//alert(d.getHours());
+//alert(d.getMinutes());
+
+
+
+//var count = 0;
+//for(count= 0; count <=5; count++){
+ //   alert(count);
+//};
+
+
+
+
+
+
+//var count = 0;
+//while(count <= 5){
+ //   console.log(count);
+ //   alert(count);
+ //   count++;
+//}
+
+
+
+
+//var idade = prompt("qual sua idade");
+//if (idade >= 18){
+ //   alert("maior de idade");
+  //  }else{
+    //    alert("menor de idade");
+   // };
+
+
+
+
+//var nome = "caique cruz";
+//var n1 = 25;
+//var n2 = 24;
+//var frase = "santos é o melhor time do mundo";
+//alert(nome+ " tem" + idade + "anos")
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.replace("santos", "santos"));
+//alert(frase.replace("nome", "outro nome")); 
